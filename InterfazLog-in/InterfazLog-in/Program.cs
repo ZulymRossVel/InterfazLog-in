beb0075012e0cc802147a4cc43ae18c6051d01b3
ZulymRossVel <71784771+ZulymRossVel@users.noreply.github.com>
2020-10-26 20:57:10 -0600

    Prueba 2
    Aun no funciona pero ahorita vemos que pex

--- InterfazLog-in/InterfazLog-in/Program.cs
+++ InterfazLog-in/InterfazLog-in/Program.cs
@@ -1,14 +1,80 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
-namespace InterfazLog_in
+namespace InterfazLog-in
 {
-    class Program
+    public partial class Form1 : InterfazLog
     {
-        static void Main(string[] args)
+        public Form1()
         {
-            Console.WriteLine("Hello World!");
-            private void
+            InitializeComponent();
+        }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void login()
+        {
+            string connect = "dataSource=localhost ; port=3306; username=root; password=; database=loginbase";
+            string query = "select * from login where user = '" + usertextbox.Text + "' AND password = '" + pswtextbox.Text + "'";
+            MySqlConnection databaseConnection = new MySqlConnection(connect);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+            try
+            {
+                databaseConnection.Open();
+                reader = commandDatabase.ExecuteReader();
+                if (reader.Read())
+                {
+                    MessageBox.Show("Usuario y contraseña correctos, bienvenido");
+                }
+                else
+                {
+                    MessageBox.Show("Datos erroneos, intente de nuevo");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void usertextbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pswtextbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginbutton_Click(object sender, EventArgs e)
+        {
+            login();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 register = new Form2();
+            register.Show();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form2 register = new Form2();
+            register.Show();
         }
     }
 }
